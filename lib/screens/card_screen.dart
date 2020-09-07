991ea631637d2c7a6a1ca04130c479745de90ad4
Trevor Pendarvis <tpendarvis@uco.edu>
2020-09-07 14:00:44 -0500

    added elements to card screen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment1_Creative_Work/screens/start_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,46 @@ class CardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Card Screen Appbar"),
       ),
-      body: Text("This is the body"),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 200.0),
+        child: Card(
+          color: Colors.blue[600],
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset("images/Pc2.jpg"),
+                title: Text("New Computer"),
+                subtitle: Text("Sale only here for one day"),
+                hoverColor: Colors.orange,
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () => print("Computer bought"),
+                    child: Text(
+                      "Buy Now!",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    color: Colors.white,
+                    splashColor: Colors.teal[900],
+                  ),
+                  FlatButton(
+                    onPressed: () =>
+                        Navigator.pushNamed(context, StartScreen.routeName),
+                    child: Text(
+                      "Place on lay away",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    color: Colors.white,
+                    splashColor: Colors.teal[900],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
