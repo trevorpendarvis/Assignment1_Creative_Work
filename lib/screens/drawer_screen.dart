f1d238fe2827130738e627f3ab6b2ec2c31cdbc0
Trevor Pendarvis <tpendarvis@uco.edu>
2020-09-07 14:43:14 -0500

    added elements to drawer screen/ finished app

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assignment1_Creative_Work/screens/start_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,67 @@ class DrawerScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Drawer AppBar"),
       ),
-      body: Text("this is the body"),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.black),
+              child: Text(
+                "Header",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text(
+                "Messages",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                "Profile",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text(
+                "Balance",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: FlatButton(
+                onPressed: () =>
+                    Navigator.pushNamed(context, StartScreen.routeName),
+                child: Text(
+                  "Exit",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                color: Colors.blue[600],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
